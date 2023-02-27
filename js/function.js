6a6407af8d2f3d7bf96296fd3289c35b47dc7008
gabitagrass <123170479+gabitagrass@users.noreply.github.com>
2023-02-26 22:16:46 -0300

    Segundo push
    Este es mi proyecto final de Argentina Programa.

---
+++ js/function.js
@@ -0,0 +1,47 @@
+
+ document.getElementById('boton').onclick=function(){
+    console.log("Capturamos el elemento Click")
+ }
+ 
+ document.getElementById('boton1').onclick=function () {
+    console.log("Capturamos el elemento Click")
+    document.getElementById("about").innerHTML ="** Professional Photographer. From Athens (Greece) **";
+    
+
+  }
+
+document.getElementById('boton2').onclick=function () {
+    console.log("Capturamos el elemento Click")
+    document.getElementById("about").innerHTML ="* Professionnel de la photographie. De Atenas (Grèce) *";
+    
+         
+}
+
+document.getElementById('boton3').onclick=function () {
+    console.log("Capturamos el elemento Click")
+    document.getElementById("about").innerHTML =" *** Fotografo professionale. Da Atene (Grecia) .  *****";
+
+}
+document.getElementById('boton4').onclick=function () {
+    console.log("Capturamos el elemento Click")
+    document.getElementById("about").innerHTML =" ***** Fotógrafa profesional. De Atenas (Grecia). ******* ";
+
+}
+
+document.getElementById('boton5').onclick=function () {
+    console.log("Capturamos el elemento Click")
+    document.getElementById("about").innerHTML =" ****** Fotografo profissional. De Atenas (Grécia).  ******";
+
+}
+document.getElementById('boton6').onclick=function () {
+    console.log("Capturamos el elemento Click")
+    document.getElementById("about").innerHTML =" * Casilla de correo electrónico: pallasathena@gmail.com **";
+}
+document.getElementById('boton7').onclick=function () {
+    console.log("Capturamos el elemento Click")
+    document.getElementById("about").innerHTML =" * Phone number -Nro de Teléfono personal: 666 666 6666  *";
+
+}
+
+
+
